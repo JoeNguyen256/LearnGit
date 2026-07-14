679a24bee3d85c167a3b26b35daa2877d24c8de8
Nguyễn Minh Khoa <minhkhoa250699@gmail.com>
2026-07-14 20:20:51 +0700

    Gem Catcher

---
+++ Assets/Prefabs/GemMoverandSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemMoverandSound : MonoBehaviour
+{
+    public float speed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime); //tạo chuyển động theo phương thẳng đứng hướng xuống với tốc độ trên theo thời gian
+    }
+
+    void OnTriggerEnter2D(Collider2D other) //Hàm này Unity tự động gọi khi có collider khác chạm vào trigger của viên ngọc. Tham số other chính là "người vừa đụng vào tôi".
+    {
+        if (other.gameObject.CompareTag("Player")) // thiết lập điều kiện kiểm tra thông tin của OTHER
+                                                   // nếu, phương thức so sánh gameobject tag của other với nhãn "Player" là đúng thì
+        {
+            AudioSource audioSource = other.GetComponent<AudioSource>(); //Khai báo biến tên audioSource để gán thông tin và các hàm của audio component từ lệnh other.GetComponent<AudioSource>()
+            audioSource.Play(); //play âm thanh từ component đó
+            //  âm thanh phát từ Player chứ không phải từ ngọc. Đây là thiết kế thông minh — vì nếu loa gắn trên ngọc, Destroy(gameObject) sẽ xóa luôn cả loa trước khi kịp phát hết tiếng. Giống như chuông cửa: chuông gắn trong nhà (Player), không gắn trên ngón tay khách (gem) — khách đi rồi chuông vẫn kêu xong.
+            Destroy(gameObject); //xóa gameObject đang gắn collider này. (Không phải là other)
+                                 //nghĩa là xóa viên ngọc này, không phải xóa người chơi đang va chạm
+            ScoreManager.AddScore(1);
+        }
+        else if (other.gameObject.CompareTag("Ground")) // còn không thì, nếu là mặt đất thì
+        {
+            Destroy(gameObject); //xóa gameObject đang gắn collider này. (Không phải là other)
+                                 //nghĩa là xóa viên ngọc này, không phải xóa mặt đất
+        }
+    }
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Text Mesh Pro
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    // tạo một biến số điểm bắt đầu bằng 0 để lưu giá trị điểm của người chơi
+    public static int score = 0; // static nghĩa là gì: biến này thuộc về class ScoreManager, không thuộc về 1 instance cụ thể nào. Dù trong scene có bao nhiêu object gắn script này, tất cả đều share chung 1 giá trị, có thể gọi trực típ ở script khác
+    public TextMeshProUGUI scoreText; // tạo một biến thuộc kiểu TextMeshProUGUI tên scoreText và có thể truy cập từ Unity Editor, kéo text từ canvas vào
+    public float remainingTime; // lưu thời gian còn lại, Unity xử lý thời gian theo số thực nên dùng float
+    public GameObject gameOverPanel; //panel hiện khi thua
+    public TextMeshProUGUI gameOverText;
+    private bool isGameOver = false; // nhưng 1 công tắc ban đầu false -> game end true
+    // khai báo một hàm dành cho lớp ScoreManager nhằm tăng số điểm của người chơi
+    public void RestartGame() // hàm để đc button gọi
+    {
+        Time.timeScale = 1f;      // Chạy lại thời gian vì torn gameover có Time.timeScale = 0f
+        score = 0;                // Reset điểm
+        isGameOver = false;       // Reset trạng thái game over
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Load lại chính Scene hiện tại.
+    }
+    public static void AddScore(int amount) //(int amount) nghĩa là hàm sẽ chỉ nhận giá trị là integer, và giá trị này sẽ được gán vào biến có tên amount
+    {
+        score += amount; //tăng điểm theo giá trị của amount được truyền vào tại sự kiện gọi AddScore
+    }
+    private void GameOver()
+    {
+        if (isGameOver) return; // biến này giúp gameover chạy đúng 1 lần khi end, ko sẽ chạy hàng trăm lần , nếu game đã GO thoát lun
+
+        isGameOver = true; // thông báo game end
+        gameOverText.text = "Game Over!\nScore: " + score; // hiện text \n là xún dòng
+        gameOverPanel.SetActive(true); // hiện panel
+
+        Time.timeScale = 0f; // Dừng game
+    }
+    void Start() // chạy 1 lần khi game start
+    {
+        Time.timeScale = 1f; // mở thời gian
+        score = 0; // reset điểm
+        isGameOver = false; // reset Gameover
+        gameOverPanel.SetActive(false); // ẩn panel
+        remainingTime = 20f; //thời gian còn lại tại thời điểm bắt đầu bằng 30s (thời lượng của trò chơi)
+        StartCoroutine(CountdownTimer());
+        // Điều này tạo một "luồng đếm giờ" chạy song song với game. Trong khi người chơi vẫn điều khiển nhân vật và nhặt gem, Coroutine cứ mỗi giây sẽ giảm remainingTime đi 1.
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // cập nhật điểm hiển thị trên UI
+        scoreText.text = "Score: " + score + " | Time: " + Mathf.CeilToInt(remainingTime); // -> Score: 10 hoặc Score: 5 ...
+        //Mathf.CeilToInt luôn làm tròn số nguyên dương
+        if (remainingTime <= 0 && !isGameOver) // ! đã ngược giá trị = !isGameOver = !false = true
+        {
+            GameOver();
+        }
+    }
+    private IEnumerator CountdownTimer() // IEnumerator dùng tạo Coroutine (chạy, tạm dùng, chạy tiếp)
+    {
+        while (remainingTime > 0) // loop khi nào còn time vẫn chạy
+        {
+            yield return new WaitForSeconds(1f); // trả lại giá trị mới sau khi chờ 1s
+            remainingTime--; // giảm thời gian
+        }
+    }
+}
